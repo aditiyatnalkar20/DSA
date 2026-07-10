ee2844fbadf398b57f27f4732d9df743875e4f35
Aditi Yatnalkar <aditiyatnalkar15@gmail.com>
2026-07-10 18:03:10 +0530

    Added Java Basics

---
+++ incometax.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class incometax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Income:");
+        double income = sc.nextInt();
+        double tax;
+        if (income < 500000) {
+            tax = 0;
+            System.out.println("No Tax");
+        } else if (income >= 500000 && income < 1000000) {
+            tax = income * (0.2);
+            System.out.print("the tax is " + tax);
+        } else {
+            tax = income * 0.3;
+            System.out.print("the tax is :" + tax);
+        }
+        sc.close();
+    };
+}
---
+++ largestof2.java
@@ -0,0 +1,12 @@
+public class largestof2 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 15;
+        if (A >= B) {
+            System.out.println("A is largest");
+        } else {
+            System.out.println("B is largest");
+        }
+        ;
+    };
+};
